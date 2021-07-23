efaf9a89612c9164d17c343d1b2eabfa2bbe4f1f
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-07-23 19:04:11 -0300

    router done

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,11 @@
 import express from "express";
 import cors from "cors";
+import routes from "./routes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-import * as recommendationsController from "./controllers/recommendationsController";
-import * as genresController from "./controllers/genresController";
-
-app.post("/recommendations", recommendationsController.newRecommendation);
-app.post("/recommendations/:id/upvote", recommendationsController.upvoteRecommendation);
-app.post("/recommendations/:id/downvote", recommendationsController.downvoteRecommendation);
-app.get("/recommendations/random", recommendationsController.randomRecommendation);
-app.get("/recommendations/top/:amount", recommendationsController.topRecommendations);
-
-
-app.post("/genres", genresController.newGenre);
-app.get("/genres", genresController.getAllGenres);
-app.get("/genres/:id", genresController.getGenre);
-app.get("/recommendations/genres/:id/random", genresController.randomRecommendationByGenre);
+app.use("/", routes);
 
 export default app;
